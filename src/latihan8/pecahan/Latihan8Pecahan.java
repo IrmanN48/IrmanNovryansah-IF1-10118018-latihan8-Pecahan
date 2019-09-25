079d4eec5f057303179b9dc485218e6e61bc12b9
IRMAN NOVRYANSAH <55683592+IrmanN48@users.noreply.github.com>
2019-09-25 22:48:54 +0700

    Add files via upload

---
+++ src/latihan8/pecahan/Latihan8Pecahan.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package latihan8.pecahan;
+
+/**
+ *
+ * @author Aero
+ * NAMA     : Irman Novryansah
+ * KELAS    : IF-1
+ * NIM      : 10118018
+ * Deskripsi Program : Program ini untuk menampilkan value pecahan yang sudah
+ *                      ditentukan
+ */
+public class Latihan8Pecahan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        //Deklarasi Variabel
+        float f;
+        double d;
+        
+        
+        //Menambahkan nilai ke variabel
+        f = 1234567890.123456789F;
+        d = 1_234_567_890.123456789;
+        
+        //Tampilan Output
+        
+        System.out.println("Float : " + f);
+        System.out.println("Double  " + d);
+   }
+    
+}
